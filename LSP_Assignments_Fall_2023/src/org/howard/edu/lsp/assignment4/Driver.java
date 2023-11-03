d29923fd632929fca43b3bce35af98c23b410c93
andrejwilliams <71712138+andrejwilliams@users.noreply.github.com>
2023-11-02 22:11:22 -0400

    assignment4 commit

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment4/Driver.java
@@ -0,0 +1,25 @@
+package org.howard.edu.lsp.assignment4;
+
+public class Driver {
+    public static void main(String[] args) {
+        IntegerSet set1 = new IntegerSet();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        System.out.println("Value of Set 1 is: " + set1.toString());
+        System.out.println("Smallest value in Set 1 is: " + set1.smallest());
+        System.out.println("Largest value in Set 1 is: " + set1.largest());
+
+        IntegerSet set2 = new IntegerSet();
+        set2.add(4);
+        set2.add(5);
+
+        System.out.println("Union of Set 1 and Set2");
+        System.out.println("Value of Set 1 is: " + set1.toString());
+        System.out.println("Value of Set 2 is: " + set2.toString());
+        set1.union(set2);
+        System.out.println("Result of union of Set 1 and Set 2: " + set1.toString());
+    }
+}
+
---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -0,0 +1,106 @@
+package org.howard.edu.lsp.assignment4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerSet {
+    private List<Integer> set = new ArrayList<Integer>();
+
+    public IntegerSet() {
+    }
+
+    public IntegerSet(ArrayList<Integer> set) {
+        this.set = set;
+    }
+
+    public void clear() {
+        set.clear();
+    }
+
+    public int length() {
+        return set.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IntegerSet otherSet = (IntegerSet) o;
+        return set.containsAll(otherSet.set) && otherSet.set.containsAll(set);
+    }
+
+    public boolean contains(int value) {
+        return set.contains(value);
+    }
+
+    public int largest() {
+        if (set.isEmpty()) {
+            throw new RuntimeException("Set is empty");
+        }
+        int largest = set.get(0);
+        for (int value : set) {
+            if (value > largest) {
+                largest = value;
+            }
+        }
+        return largest;
+    }
+
+    public int smallest() {
+        if (set.isEmpty()) {
+            throw new RuntimeException("Set is empty");
+        }
+        int smallest = set.get(0);
+        for (int value : set) {
+            if (value < smallest) {
+                smallest = value;
+            }
+        }
+        return smallest;
+    }
+
+    public void add(int item) {
+        if (!set.contains(item)) {
+            set.add(item);
+        }
+    }
+
+    public void remove(int item) {
+        set.remove((Integer) item);
+    }
+
+    public void union(IntegerSet intSetb) {
+        for (int value : intSetb.set) {
+            if (!set.contains(value)) {
+                set.add(value);
+            }
+        }
+    }
+
+    public void intersect(IntegerSet intSetb) {
+        set.retainAll(intSetb.set);
+    }
+
+    public void diff(IntegerSet intSetb) {
+        set.removeAll(intSetb.set);
+    }
+
+    public void complement(IntegerSet intSetb) {
+        List<Integer> complement = new ArrayList<>();
+        for (int value : intSetb.set) {
+            if (!set.contains(value)) {
+                complement.add(value);
+            }
+        }
+        set = complement;
+    }
+
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return set.toString();
+    }
+}
