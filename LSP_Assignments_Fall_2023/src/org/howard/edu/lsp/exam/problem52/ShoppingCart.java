a4942ff2ae8da7048084cd8f0802b90ec6dc53d7
andrejwilliams13 <83249443+andrejwilliams13@users.noreply.github.com>
2023-12-15 22:47:14 -0500

    Final exam

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/exam/problem52/ShoppingCart.java
@@ -0,0 +1,15 @@
+// Andre Williams LSP Final Exam
+
+package org.howard.edu.lsp.exam.problem52;
+
+public class ShoppingCart {
+    private PaymentStrategy paymentStrategy;
+
+    public ShoppingCart(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
